f72d99060b3e921ea4693d00d64f7ea9382c8d45
hongge199 <1259131259@qq.com>
2020-08-21 02:37:04 +0000

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,8 @@
+<html>
+  <head>
+    <title>@yield('title','Weibo App') --自学测试项目</title>
+  </head>
+  <body>
+   @yield('content')
+  </body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title')
+@section('content')
+    <h1>主页面</h1>
+@endsection
