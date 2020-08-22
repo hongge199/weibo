2d41e026692cd9053406fd515291c2d202931b43
hongge199 <1259131259@qq.com>
2020-08-22 06:58:20 +0000

    用户界面显示、头像调用

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,10 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
+    public function gravatar($size = '100'){
+        $hash =md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
     /**
      * The attributes that should be hidden for arrays.
      *
---
+++ resources/views/shared/_user_info.blade.php
@@ -0,0 +1,4 @@
+<a href="{{'users.show',$user->id}}">
+<img src="{{$user->gravatar('140')}}" alt="{{$user->name}}" class="gravtar"/>
+</a>
+<h1>{{ $user->name}}</h1>
